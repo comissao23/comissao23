fe3815ac5ff8aeaeb81fc28db24145cf178fa4c1
Gabriel Rodrigues <groodrigues17@gmail.com>
2021-07-08 10:38:21 -0300

    criada LP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,99 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/simbolo_RGB-150x150.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Seja um parceiro da Comissão 2023!
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Ajude a realizar sonhos e a formar pessoas melhores!
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+        v-if="mostrarCodigo"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+         Digite o seu código aqui!
+        </h2>
+
+        <v-row justify="center">
+          <v-text-field
+            outlined
+            v-model="codigoParceiro"
+          ></v-text-field>
+        </v-row>
+        <v-row justify="center">
+          <v-btn @click="checkParceiro()" color="primary">
+            Pesquisar Código!
+          </v-btn>
+        </v-row>
+      </v-col>
+
+      <v-col
+      cols="12"
+       v-if="selectedParceiro">
+        <v-row class="mb-5" justify="center">
+          <h1>{{this.selectedParceiro.nome}}</h1>
+        </v-row>
+        <v-row class="mt-5" justify="center">
+          <h1>{{this.selectedParceiro.proposta}}</h1>
+        </v-row>
+        <v-row class="pt-5" justify="center">
+          <v-btn color="primary" @click="voltar()">
+            Voltar
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+   data() {
+     return {
+       codigoParceiro: null,
+       mostrarCodigo: true,
+       selectedParceiro: null,
+       parceiros: [
+         {
+        id: 12556,
+        nome: 'Rei Burgão',
+        proposta: 'Ajude com 1000 Reais e ganhe um banner na festa!'
+       },
+       {
+        id: 12456,
+        nome: 'Speaking',
+        proposta: 'Ajude com 4500 Reais e ganhe um banner na festa, além de copos com a logo Speaking!'
+       }
+       ]
+     }
+   },
+   methods: {
+    checkParceiro(){
+      this.selectedParceiro = this.parceiros.find(parceiro => parceiro.id == this.codigoParceiro)
+      this.mostrarCodigo = false
+      console.log(this.selectedParceiro)
+    },
+    voltar(){
+      this.mostrarCodigo = true
+      this.selectedParceiro = null
+    }
+   },
+  }
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    publicPath: '/comissao23/'
-  }
+  publicPath: '/parceiros/',
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
